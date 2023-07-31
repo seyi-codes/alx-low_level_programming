dffae56c7ee0ca00df77faf0fa9592ba0be2f49b
seyi-codes <samuelsomoye13@gmail.com>
2023-07-31 14:52:34 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,12 +4,6 @@
  * @s: input
  * @c: input
  * Return: Always 0 (Success)
-#include "main.h"
-/**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
